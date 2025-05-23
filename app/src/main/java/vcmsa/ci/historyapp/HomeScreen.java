307c3f078e3084b240bf1b4599d7970461ba57e8
lab_services_student <st10478498@vconnect.edu.za>
2025-05-23 10:47:23 +0200

    first push, completed the  main activity frontend and backend

---
+++ app/src/main/java/vcmsa/ci/historyapp/HomeScreen.java
@@ -0,0 +1,6 @@
+package vcmsa.ci.historyapp;
+
+import android.app.Activity;
+
+public class HomeScreen extends Activity {
+}
---
+++ app/src/main/java/vcmsa/ci/historyapp/MainActivity.kt
@@ -0,0 +1,31 @@
+package vcmsa.ci.historyapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        //declare the variable for the button
+
+        val startButton = findViewById<Button>(R.id.startButton)
+
+        //set a set on click listener that will trigger the button to work
+
+        startButton.setOnClickListener {
+            val intent = Intent(this, QuestionsScreen::class.java)
+
+            //This will trigger the screen to move to the next screen.
+            startActivity(intent)
+        }
+
+    }
+}
