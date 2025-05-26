c2e19d828eeb44a657cc4c9e1b4b155dc588d899
lab_services_student <st10478498@vconnect.edu.za>
2025-05-26 13:00:07 +0200

    finished coding the score screen

--- app/src/main/java/vcmsa/ci/historyapp/ScoreScreen.kt
+++ app/src/main/java/vcmsa/ci/historyapp/ScoreScreen.kt
@@ -1,40 +1,74 @@
 package vcmsa.ci.historyapp
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.content.ContextCompat
 
 class ScoreScreen : AppCompatActivity() {
+
     private lateinit var scoreText: TextView
-    private lateinit var feedbackText:TextView
-    private lateinit var reviewButton:Button
-    private lateinit var exitButton:Button
+    private lateinit var feedbackText: TextView
+    private lateinit var reviewButton: Button
+    private lateinit var exitButton: Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_score_screen)
 
-
-        //Initialise all all variables.
-        scoreText = findViewById(R.id.ScoreText)
+        // Initialise all variables
+        scoreText = findViewById(R.id.scoreText)
         feedbackText = findViewById(R.id.feedbackText)
         reviewButton = findViewById(R.id.ReviewButton)
         exitButton = findViewById(R.id.exitButton)
 
-        //getting the score from the intent
-        val score = intent.putExtra("SCORE",0)
+        // Get the score passed from the previous activity
+        val score = intent.getIntExtra("SCORE", 0)
 
-        //This is the total fixed number of questions
+        // This is the total fixed number of questions
         val totalQuestions = 5
 
-        //Display the score
-        scoreText.text = "You Got $score Out Of $totalQuestions Correct!"
+        // Display the score
+        scoreText.text = "You got $score out of $totalQuestions correct!"
 
+        // Show the personalised feedback
+        if (score > 3) {
+            feedbackText.text = "GREAT JOB!"
+            feedbackText.setTextColor(ContextCompat.getColor(this, android.R.color.holo_blue_dark))
+        } else {
+            feedbackText.text = "KEEP PRACTICING!"
+            feedbackText.setTextColor(ContextCompat.getColor(this, android.R.color.holo_red_dark))
+        }
+
+        // Set listener for the Review Button
+        reviewButton.setOnClickListener {
+            // TODO: Navigate to Review Screen
+        }
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
